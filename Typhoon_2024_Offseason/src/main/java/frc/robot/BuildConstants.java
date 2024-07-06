cc043cfc7963c2020fdb09ba67cd1057896e8de1
Roman-Tech-Plus <rrcabrahamson@gmail.com>
2024-07-06 00:08:33 -0700

    ...

--- Typhoon_2024_Offseason/src/main/java/frc/robot/BuildConstants.java
+++ Typhoon_2024_Offseason/src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Typhoon_2024_Offseason";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 5;
-  public static final String GIT_SHA = "1368976c75483dde404f43bd175f6ee7a860b33a";
-  public static final String GIT_DATE = "2024-06-29 01:30:20 PDT";
+  public static final int GIT_REVISION = 6;
+  public static final String GIT_SHA = "d517e538316d063e41d3ef4aa2b8d42b0fabe99c";
+  public static final String GIT_DATE = "2024-07-05 01:45:29 PDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2024-07-05 01:28:17 PDT";
-  public static final long BUILD_UNIX_TIME = 1720168097918L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2024-07-05 19:25:48 PDT";
+  public static final long BUILD_UNIX_TIME = 1720232748002L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
--- Typhoon_2024_Offseason/src/main/java/frc/robot/subsystems/vision/ZED.java
+++ Typhoon_2024_Offseason/src/main/java/frc/robot/subsystems/vision/ZED.java
@@ -7,13 +7,17 @@ package frc.robot.subsystems.vision;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.geometry.Twist2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.util.struct.Struct;
 import edu.wpi.first.util.struct.StructDescriptor;
 import edu.wpi.first.util.struct.StructSerializable;
@@ -22,8 +26,8 @@ import frc.robot.BreakerLib.physics.BreakerVector3;
 
 public class ZED extends SubsystemBase {
   /** Creates a new ZED. */
-  public ZED() {
-    
+  public ZED(Function<Double, Pose2d> robotPoseAtTimeFunc, Function<Double, ChassisSpeeds> chassisSpeedsAtTimeFunc) {
+
   }
 
   private ArrayList<TrackedObject> trackedObjects;
@@ -36,10 +40,26 @@ public class ZED extends SubsystemBase {
     return null;
   }
 
+  public void test() {
+    Transform3d robotVelsInWorldFrame = new Transform3d();
+    Transform3d robotToCamera= new Transform3d();
+    Translation3d objVelsInCameraFrame = new Translation3d();
+
+    Translation3d cross = robotToCamera.rotateBy(robotVelsInWorldFrame.getRotation());
+    Translation3d v_worldToObj = robotVelsInWorldFrame.getTranslation().plus(cross).plus(objVelsInCameraFrame);
+  }
+
+
+
   public static final record ObjectDimensions(double width, double height, double length) {}
+
+  public static final class ObjectMotion {
+    public ObjectMotion(double timestamp, BreakerVector3 observedCameraRelitiveObjectMotion, Transform3d robotToCameraTransform, Pose3d globalRobotPose)
+  }
 
   public static final class ObjectPosition {
     public ObjectPosition(double timestamp, BreakerVector3 cameraRelivitveObjectVelocity, Translation3d cameraToObjectTranslation, Transform3d robotToCameraTransform, Pose2d globalRobotPose) {
+
       
     }
 
