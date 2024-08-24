00a6a183af45fd5dac85228a409533da51d36f05
BreakerBots <132958980+BreakerBots-public@users.noreply.github.com>
2024-08-24 14:28:28 -0700

    Update BuildConstants.java

--- Typhoon_2024_Offseason/src/main/java/frc/robot/BuildConstants.java
+++ Typhoon_2024_Offseason/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Typhoon_2024_Offseason";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 9;
-  public static final String GIT_SHA = "270589bed41c9dde6659b0f29706adf21b49e85f";
-  public static final String GIT_DATE = "2024-07-27 16:42:02 PDT";
+  public static final int GIT_REVISION = 12;
+  public static final String GIT_SHA = "c24d0feb79115d38ba02b106c4f513615cc8e0f6";
+  public static final String GIT_DATE = "2024-08-23 08:38:00 PDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2024-08-01 22:25:59 PDT";
-  public static final long BUILD_UNIX_TIME = 1722576359794L;
+  public static final String BUILD_DATE = "2024-08-24 12:46:18 PDT";
+  public static final long BUILD_UNIX_TIME = 1724528778480L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
